f2b36e3e6a49f7dcecb8ef91a6dcbf431baf7196
Ajay <ajay.rawat1792000@gmail.com>
2023-10-18 12:35:23 +0530

    initial commit

---
+++ src/com/phone_book/project/Validate.java
@@ -0,0 +1,15 @@
+package com.phone_book.project;
+
+public class Validate extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 428829246218125194L;
+	
+	public Validate(String s) {
+		super(s);
+	}
+		
+	}
+
